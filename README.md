cb0d196ccd14cf484761cfdc684c1c29bfb3963c
JeffDinsmore <hello@jeffdinsmore.com>
2020-10-22 20:49:04 -0700

    added readme and updated html/css and js files

---
+++ README.md
@@ -0,0 +1,36 @@
+# _Madlib Form_
+
+#### _Website form to reveal a madlib after entering , 08/12/2020_
+
+#### By _**Jeff Dinsmore**_
+
+## Description
+
+_This website is a Madlib form to enter words into and reveal your madblib with a button click._
+
+## Setup/Installation Requirements
+
+* _Clone this repository._
+* _In the cloned folder, double click on index or index.html. If the file doesn't open, right click on index or index.html and "open with" your browser of choice._
+
+## Known Bugs
+
+_No known bugs at this time._
+
+## Support and contact details
+
+_Any known issues, please reach out to Jeff Dinsmore by email at hello@jeffdinsmore.com._
+
+## Technologies Used
+
+* _HTML_
+* _CSS_
+* _JavaScrip_
+* _jQuery_
+* _Bootstrap_
+
+### License
+
+Copyright (c) 2020 **_Jeff Dinsmore_**
+
+This software is licensed under the MIT license.
--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,34 @@
-$(document).ready(function() {
-  $("#formOne").submit(function(event){
-   const pluralNoun1Input = $("input#pluralNoun1").val();
-   const pluralNoun2Input = $("input#pluralNoun2").val();
-   const pluralNoun3Input = $("input#pluralNoun3").val();
-   const pluralNoun4Input = $("input#pluralNoun4").val();
-   const verb1Input = $("input#verb1").val();
-   const noun1Input = $("input#noun1").val();
-   const noun2Input = $("input#noun2").val();
-   const ingVerbInput = $("input#ingVerb").val();
-   
-   $(".pluralNoun1").append(pluralNoun1Input);
-   $(".pluralNoun2").append(pluralNoun2Input);
-   $(".pluralNoun3").append(pluralNoun3Input);
-   $(".pluralNoun4").append(pluralNoun4Input);
-   $(".verb1").append(verb1Input);
-   $(".noun1").append(noun1Input);
-   $(".noun2").append(noun2Input);
-   $(".ingVerb").append(ingVerbInput);
+$(document).ready(function () {
+  $("#formOne").submit(function (event) {
+    const pluralNoun1Input = $("input#pluralNoun1").val();
+    const pluralNoun2Input = $("input#pluralNoun2").val();
+    const pluralNoun3Input = $("input#pluralNoun3").val();
+    const pluralNoun4Input = $("input#pluralNoun4").val();
+    const verb1Input = $("input#verb1").val();
+    const noun1Input = $("input#noun1").val();
+    const noun2Input = $("input#noun2").val();
+    const ingVerbInput = $("input#ingVerb").val();
 
-   $(".pluralNoun1").text(pluralNoun1Input);
-   $(".pluralNoun2").text(pluralNoun2Input);
-   $(".pluralNoun3").text(pluralNoun3Input);
-   $(".pluralNoun4").text(pluralNoun4Input);
-   $(".verb1").text(verb1Input);
-   $(".noun1").text(noun1Input);
-   $(".noun2").text(noun2Input);
-   $(".ingVerb").text(ingVerbInput);
+    $(".pluralNoun1").append(pluralNoun1Input);
+    $(".pluralNoun2").append(pluralNoun2Input);
+    $(".pluralNoun3").append(pluralNoun3Input);
+    $(".pluralNoun4").append(pluralNoun4Input);
+    $(".verb1").append(verb1Input);
+    $(".noun1").append(noun1Input);
+    $(".noun2").append(noun2Input);
+    $(".ingVerb").append(ingVerbInput);
 
-   $(".story").show();
-   event.preventDefault();
+    $(".pluralNoun1").text(pluralNoun1Input);
+    $(".pluralNoun2").text(pluralNoun2Input);
+    $(".pluralNoun3").text(pluralNoun3Input);
+    $(".pluralNoun4").text(pluralNoun4Input);
+    $(".verb1").text(verb1Input);
+    $(".noun1").text(noun1Input);
+    $(".noun2").text(noun2Input);
+    $(".ingVerb").text(ingVerbInput);
+
+    $(".story").show();
+    event.preventDefault();
   });
 
 });
